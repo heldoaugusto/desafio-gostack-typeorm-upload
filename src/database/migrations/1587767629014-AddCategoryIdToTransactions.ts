d552e22403b4d2031d18521c68bbeb39f7f34c2b
Heldo Augusto <heldoaugusto@gmail.com>
2020-04-24 19:42:17 -0300

    Create Migrations

---
+++ src/database/migrations/1587767629014-AddCategoryIdToTransactions.ts
@@ -0,0 +1,38 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddCategoryIdToTransactions1587767629014
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        columnNames: ['category_id'],
+        referencedTableName: 'categories',
+        referencedColumnNames: ['id'],
+        name: 'TransactionCategory',
+        onUpdate: 'cascade',
+        onDelete: 'set null',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'TransactionCategory');
+
+    await queryRunner.dropColumn('transactions', 'category_id');
+  }
+}
